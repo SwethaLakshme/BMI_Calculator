c4d200a45d457de9f49b1f0597e3e448fa0868d3
Swetha Lakshme S <swethalakshme02@gmail.com>
2021-07-15 20:31:03 +0530

    BMI displayed

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
     var Weight = prompt("Enter your weight in kg");
     var Height = prompt("Enter your height in m")
     var bmi = Weight/(Math.pow(Height,2));
-
+    
     var h2 = document.createElement('h2');
+    var bmiAnswer = document.createTextNode(' and your BMI is: '+bmi.toFixed(2));
     if(bmi<18.5)
     {
       var TextAnswer = document.createTextNode('Underweight');
@@ -21,6 +22,7 @@
     }
     h2.setAttribute('id','BMICalculator');
     h2.appendChild(TextAnswer);
+    h2.appendChild(bmiAnswer);
     document.getElementById('flex-box-result').appendChild(h2);
  }
 
