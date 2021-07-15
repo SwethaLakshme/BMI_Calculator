8b8cada545f71adf6d36b29220cf2f49b187e818
Swetha Lakshme S <swethalakshme02@gmail.com>
2021-07-15 20:12:25 +0530

    BMI Calculator

---
+++ script.js
@@ -0,0 +1,29 @@
+ function BMICalculator(){ 
+    var Weight = prompt("Enter your weight in kg");
+    var Height = prompt("Enter your height in m")
+    var bmi = Weight/(Math.pow(Height,2));
+
+    var h2 = document.createElement('h2');
+    if(bmi<18.5)
+    {
+      var TextAnswer = document.createTextNode('Underweight');
+    }
+    else if( bmi>=18.5 && bmi<25)
+    {
+      var TextAnswer = document.createTextNode('Normal');
+    }
+    else if( bmi>=25 && bmi<30)
+    {
+      var TextAnswer = document.createTextNode('Overweight');
+    }
+    else if(bmi>30){
+      var TextAnswer = document.createTextNode('Obesity');
+    }
+    h2.setAttribute('id','BMICalculator');
+    h2.appendChild(TextAnswer);
+    document.getElementById('flex-box-result').appendChild(h2);
+ }
+
+ function reset(){
+    document.getElementById('BMICalculator').remove();
+ }
